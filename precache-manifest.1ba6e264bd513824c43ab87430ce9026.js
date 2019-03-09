50550e32a5d9f4bacc10dd77744b6d137787415a
Jaypee Ignacio <jaypeeig@gmail.com>
2019-03-09 22:20:38 +0800

    Updates

---
+++ precache-manifest.1ba6e264bd513824c43ab87430ce9026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d96fcce83cd6c30e6a3d",
+    "url": "/static/js/main.41c7fcd7.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "d96fcce83cd6c30e6a3d",
+    "url": "/static/css/main.2b3f9f07.chunk.css"
+  },
+  {
+    "revision": "16dbaf73d8a65c7714e7e77f848ecad9",
+    "url": "/index.html"
+  }
+];
